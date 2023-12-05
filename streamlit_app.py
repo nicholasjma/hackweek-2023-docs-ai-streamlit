6af76ebed2320598ead675409d66ce9c4527f6cf
Nick Ma <nick.ma@iterable.com>
2023-12-04 20:39:27 -0600

    layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def generate_response(input_text):
     markdown = response["result"]
     markdown += "\n\n*Sources*\n\n"
     for n, source in enumerate(response["sources"], start=1):
-        markdown += f"{n}. [{source['page_title']}]({source['url']})\n\n"
+        markdown += f"{n}. [{source['page_title']}]({source['url']})  "
     container.write(markdown)
     with st.expander("Search Results", expanded=False):
         for n, search_result in enumerate(response["search_results"], start=1):
