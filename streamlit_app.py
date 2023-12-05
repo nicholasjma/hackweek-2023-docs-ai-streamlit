74c5bae3637a7fff02b96a8348cce154f38f5d20
Nick Ma <nick.ma@iterable.com>
2023-12-04 20:52:12 -0600

    layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.markdown(
     """
         <style>
                .block-container {
-                    padding-top: 5rem;
+                    padding-top: 0rem;
                     padding-bottom: 0rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
